80c4c767791467a5b4e8ee297626735a372b80b0
taylor <taylormyers01@comcast.net>
2023-07-14 16:59:55 -0400

    This was a super good lab. Helped me understand comparators and made them make sense. Thank you

--- src/main/java/rocks/zipcode/io/comparators/PriceComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/PriceComparator.java
@@ -1,7 +1,22 @@
 package rocks.zipcode.io.comparators;
 
+import rocks.zipcode.io.Item;
+
+import java.util.Comparator;
+
 /**
  * @author leon on 30/01/2019.
  */
-public class PriceComparator {
+public class PriceComparator implements Comparator<Item> {
+
+    public int compare(Item a, Item b){
+        if(a.getPrice()-b.getPrice()< 0){
+            return -1;
+        } else if (a.getPrice()-b.getPrice() > 0) {
+            return 1;
+        }
+        else{
+            return (int)(a.getPrice()-b.getPrice());
+        }
+    }
 }
